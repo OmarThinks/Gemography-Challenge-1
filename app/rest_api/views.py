2763cf75ecaf8301cad2a9730154d22cda5ac4e9
Omar Magdy <contact.parmag@gmail.com>
2021-05-28 23:47:29 +0200

    views.py:add:github search repo

--- app/rest_api/views.py
+++ app/rest_api/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
 
 from .serializers import (GithubSearchRepoSerializer, build_queries)
+
+import requests
+
+
+
+def (queries):
+	# get the response from the URL
+	responses = []
+	for q in queriesq:
+		responses.append(requests.get('query'))
+	#result = do_something_with_response(response)
+	return responses
+
+
 
 
 @api_view(['GET'])
@@ -10,6 +25,11 @@ def github_search_repo(request):
 	q_params = request.query_params
 	print(q_params)
 	ser = GithubSearchRepoSerializer(**q_params)
-	if ser.is_valid():
-		return Response(ser.save)
-	return Response({"message": "Hello for today! See you tomorrow!"})
+	responses
+	if not ser.is_valid():
+    	content = ser.errors
+    	return Response(content, 
+    		status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    return Response({"message": "Hello for today! See you tomorrow!"})
+
+
