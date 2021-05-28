ebcd8f97467971e3a91004c2237d0d1aaf81a15b
Omar Magdy <contact.parmag@gmail.com>
2021-05-28 15:51:22 +0200

    serializer.py:add:validation of date

--- app/rest_api/serializer.py
+++ app/rest_api/serializer.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 
+from datetime import datetime
+
 class GithubSearchRepoSerializer(serializers.Serializer):
     date = serializers.DateField()
     sort = serializers.CharField(max_length=20)
     order = serializers.CharField(max_length=20)
-		
+
+    def validate_date(self, value):
+        """
+        Validate if date is more than now
+        """
+        if (value > datetime.today().date()):
+        	raise serializers.ValidationError(
+        		"the value of date can not be in the future")
+        return value
+
+    def create(self, validated_data):
+        pass
+        #return Comment(**validated_data)
