b5335295b8881acae3bd0158e87476a890921f34
Omar Magdy <contact.parmag@gmail.com>
2021-05-28 23:07:38 +0200

    views.py:add:api_view

--- app/rest_api/views.py
+++ app/rest_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .serializers import (GithubSearchRepoSerializer, build_queries)
+
+
+@api_view(['GET'])
+def github_search_repo(request):
+	q_params = request.query_params
+	print(q_params)
+	ser = GithubSearchRepoSerializer(**q_params)
+	if ser.is_valid():
+		return Response(ser.save)
+	return Response({"message": "Hello for today! See you tomorrow!"})
