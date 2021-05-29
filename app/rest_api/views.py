72bd671182bb82636190aa678521e85c373487b8
Omar Magdy <contact.parmag@gmail.com>
2021-05-29 07:42:06 +0200

    views.py:add:handle queries

--- app/rest_api/views.py
+++ app/rest_api/views.py
@@ -9,12 +9,13 @@ import requests
 
 
 
-def (queries):
+def handle_queries(queries):
 	# get the response from the URL
 	responses = []
-	for q in queriesq:
+	for q in queries:
 		responses.append(requests.get('query'))
 	#result = do_something_with_response(response)
+	print(responses[0])
 	return responses
 
 
@@ -25,11 +26,12 @@ def github_search_repo(request):
 	q_params = request.query_params
 	print(q_params)
 	ser = GithubSearchRepoSerializer(**q_params)
-	responses
+	responses = []
 	if not ser.is_valid():
-    	content = ser.errors
-    	return Response(content, 
-    		status=status.HTTP_422_UNPROCESSABLE_ENTITY)
-    return Response({"message": "Hello for today! See you tomorrow!"})
+		content = ser.errors
+		return Response(content, 
+			status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+	handle_queries(responses)
+	return Response({"message": "Hello for today! See you tomorrow!"})
 
 
