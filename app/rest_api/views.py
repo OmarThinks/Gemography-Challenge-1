2bca1f7d23b1d2fbd6c985a1af6b4a7de4c4cad8
Omar Magdy <contact.parmag@gmail.com>
2021-05-29 13:45:43 +0200

    created templates

--- app/rest_api/views.py
+++ app/rest_api/views.py
@@ -11,28 +11,18 @@ from .github_search import github_search_repos
 
 
 
-def handle_queries(queries):
-	# get the response from the URL
-	responses = []
-	for q in queries:
-		responses.append(requests.get(q))
-	print(responses[0])
-	return responses
-
-
-
-
-
 
 @api_view(['GET'])
 def github_search_repo_view(request):
 	params = request.query_params
+	#print(dict(params),flush=True)
 	q_params={}
 	for key in params:
 		q_params[key] = params[key]
+	#print(dict(q_params),flush=True)
 	result = github_search_repos(**q_params)
+	
 	if result["success"]:
 		return Response(result)
 	return Response(result, 
 			status=status.HTTP_422_UNPROCESSABLE_ENTITY)
-
