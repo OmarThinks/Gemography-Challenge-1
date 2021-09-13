529c0fcd9169ec9e28f18bd81041a27e30458b2d
Omar Magdy <contact.parmag@gmail.com>
2021-09-13 20:56:41 +0200

    views.py:mod:refractored

--- app/rest_api/views.py
+++ app/rest_api/views.py
@@ -36,70 +36,22 @@ def github_search_repo_view(request):
 
 
 
-
-"""
-
-from pprint import pp
-class CustomBrowsableAPIRenderer(BrowsableAPIRenderer):	
-	def get_context(
-		self, data, accepted_media_type, renderer_context):
-		context = BrowsableAPIRenderer.get_context(
-			self, data, accepted_media_type, renderer_context)
-		
-		try:
-			serializer = data.serializer
-			print(serializer)
-			form_renderer = self.form_renderer_class()
-			form = form_renderer.render(
-				serializer.data,
-				self.accepted_media_type,
-				{'style': {'template_pack': 'rest_framework/horizontal'}}
-			)
-			context["put_form"] = form
-			print(form)
-		except Exception as e:
-			pass
-		return context
-
-
-
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class GithubReopsViewSetAgain(viewsets.ViewSet):
 	serializer_class = GithubSearchRepoSerializer
-	renderer_classes = [
-	JSONRenderer,BrowsableAPIRenderer]
+	renderer_classes = [JSONRenderer,BrowsableAPIRenderer]
 	
 	def list(self, request):
 		serialzer = serializer = GithubSearchRepoSerializer(
 			data=request.query_params)
-		if serializer.is_valid(raise_exception=True):
-			response = Response(serializer.validated_data)
-		else:
-			response = Response(serializer.errors)
+		serializer.is_valid(raise_exception=True)
+		response = Response(serializer.validated_data)
 		return response
 
 	def create(self, request):
 		serialzer = serializer = GithubSearchRepoSerializer(
 			data=request.data)
-		if serializer.is_valid(raise_exception=True):
-			response = Response(serializer.validated_data)
-		else:
-			response = Response(serializer.errors)
+		serializer.is_valid(raise_exception=True)
+		response = Response(serializer.validated_data)
 		return response
 
 
